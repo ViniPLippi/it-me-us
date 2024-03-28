2983b79c01014b481fba5ce1c6164df980d27322
Vinicius Lippi <viniplippi@gmail.com>
2024-03-28 12:25:16 -0300

    Teste

--- start.sh
+++ start.sh
@@ -18,6 +18,6 @@ php artisan route:clear
 php artisan storage:link
 nohup php artisan queue:work --daemon &
 
-envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN -prefix=labqs/flualfa php artisan serve --port=8080
+#envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN -prefix=labqs/flualfa php artisan serve --port=8080
 
 echo "exited $0"
